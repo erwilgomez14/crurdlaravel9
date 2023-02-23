02e882c85ed4c4e1ecf025d157400ce46ea275f8
erwilgomez14 <erwildiaz@gmail.com>
2023-02-22 23:28:58 -0400

    colocando el icono de borrar

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
